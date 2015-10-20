f8d0684b564518476d0ff528f813aabd95ffec3d
gandlurm <gandlurm@gmail.com>
2015-10-20 10:39:37 +0530

    fourth commit

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,13 +1,6 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name frontendApp
- * @description
- * # frontendApp
- *
- * Main module of the application.
- */
+
 angular
   .module('frontendApp', [
     'ngAnimate',
