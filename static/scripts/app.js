5a96061d2c4162ce89f466fae4df899c822e4eec
gandlurm <gandlurm@gmail.com>
2015-10-20 11:08:55 +0530

    changes

---
+++ static/scripts/app.js
@@ -0,0 +1,24 @@
+'use strict';
+angular
+  .module('frontendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'restangular'
+    ])
+  .config(function ($routeProvider) {
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'static/views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+  
