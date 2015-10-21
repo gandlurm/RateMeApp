b995314dd7fb2abaf8e3ef01d77679c1b4bda402
gandlurm <gandlurm@gmail.com>
2015-10-21 10:56:28 +0530

    new changees3

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -13,7 +13,10 @@ angular
     'restangular'
     ])
   .config(function ($routeProvider, $locationProvider) {
-	$locationProvider.html5Mode(true);
+	$locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false
+	});
     $routeProvider
       .when('/', {
         templateUrl: 'static/views/main.html',
