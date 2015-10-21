db29fa5cd69a840b24439f38cac2d1aaf7aa6e58
gandlurm <gandlurm@gmail.com>
2015-10-21 19:28:38 +0530

    new changees3

--- static/scripts/controllers/main.js
+++ static/scripts/controllers/main.js
@@ -16,11 +16,19 @@ angular.module('frontendApp')
 	var code = $location.absUrl().substring(codeStart, codeEnd);
 	
 	console.log(code);
-	var access_code = Restangular.one('https://graph.facebook.com/v2.5/oauth/access_token?client_id=1659954354288861&redirect_uri=http://gandlurm.github.io/RateMyApp&client_secret=8d3df7692518bea961df79ea1f9939bd&code='+code);
+	
+	var access_code;
+	
+	$http.get("'https://graph.facebook.com/v2.5/oauth/access_token?client_id=1659954354288861&redirect_uri=http://gandlurm.github.io/RateMyApp&client_secret=8d3df7692518bea961df79ea1f9939bd&code='+code")
+    .success(function(response) {
+		access_code = response;
+	});
+	
+	//var access_code = Restangular.one('https://graph.facebook.com/v2.5/oauth/access_token?client_id=1659954354288861&redirect_uri=http://gandlurm.github.io/RateMyApp&client_secret=8d3df7692518bea961df79ea1f9939bd&code='+code);
 
 	console.log(access_code);
-	tours.getList().then(function(response) {
-	  $scope.tours = response;
-	  });
+	//tours.getList().then(function(response) {
+	//  $scope.tours = response;
+	//  });
 
   });
