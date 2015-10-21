24b7ecfa4cb1b8f06062470bd9e4301a20e38cbe
gandlurm <gandlurm@gmail.com>
2015-10-22 01:35:34 +0530

    new changees3

--- static/scripts/controllers/main.js
+++ static/scripts/controllers/main.js
@@ -19,7 +19,7 @@ angular.module('frontendApp')
 	
 	var access_code;
 	
-	$http.get("https://graph.facebook.com/v2.5/oauth/access_token?client_id=1659954354288861&redirect_uri=http://gandlurm.github.io/RateMyApp&client_secret=8d3df7692518bea961df79ea1f9939bd&code="+code)
+	$http.get("https://graph.facebook.com/v2.5/oauth/access_token?client_id=1659954354288861&redirect_uri=http://gandlurm.github.io/RateMeApp&client_secret=8d3df7692518bea961df79ea1f9939bd&code="+code)
     .success(function(response) {
 		access_code = response;
 		console.log(access_code.access_token);
@@ -32,7 +32,7 @@ angular.module('frontendApp')
 		});**/
 	});
 	
-	//var access_code = Restangular.one('https://graph.facebook.com/v2.5/oauth/access_token?client_id=1659954354288861&redirect_uri=http://gandlurm.github.io/RateMyApp&client_secret=8d3df7692518bea961df79ea1f9939bd&code='+code);
+	//var access_code = Restangular.one('https://graph.facebook.com/v2.5/oauth/access_token?client_id=1659954354288861&redirect_uri=http://gandlurm.github.io/RateMeApp&client_secret=8d3df7692518bea961df79ea1f9939bd&code='+code);
 
 	
 	//tours.getList().then(function(response) {
