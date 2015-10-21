3d6d5fc939c6945a6daf371a0843e808cb7e9d65
gandlurm <gandlurm@gmail.com>
2015-10-21 22:35:45 +0530

    new changees3\

--- static/scripts/controllers/ratingsPage.js
+++ static/scripts/controllers/ratingsPage.js
@@ -4,7 +4,7 @@
   'use strict';
 
   angular
-    .module('frontendApp', [])
+    .module('frontendApp')
     .controller('RatingController', RatingController)
     .directive('starRating', starRating);
 
