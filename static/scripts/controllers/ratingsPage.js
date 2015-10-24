55bfe8995cbdfa8f5c66ecf0189749d1a143def2
gandlurm <gandlurm@gmail.com>
2015-10-24 07:22:43 +0530

    Commit

--- static/scripts/controllers/ratingsPage.js
+++ static/scripts/controllers/ratingsPage.js
@@ -1,77 +1,59 @@
-// http://angulartutorial.blogspot.com/2014/03/rating-stars-in-angular-js-using.html
-
-
   'use strict';
-var starApp = angular.module('frontendApp');
 
-starApp.controller('StarCtrl', function ($scope) {
-    $scope.rating = 0;
-	
-    $scope.ratings = [{
-        current: 5,
-        max: 10
-    }];
+  angular
+    .module('frontendApp', [])
+    .controller('RatingController', RatingController)
+    .directive('starRating', starRating);
 
-    /**$scope.getSelectedRating = function (rating) {
-        console.log(rating);
-    }**/
-    
-    $scope.setMinrate= function(){
-       $scope.ratings = [{
-        current: 1,
-        max: 10
-    }];
-    }
-    
-    $scope.setMaxrate= function(){
-       $scope.ratings = [{
-        current: 10,
-        max: 10
-    }];
+  function RatingController() {
+    this.rating1 = 5;
+    this.rating2 = 2;
+    this.isReadonly = true;
+    this.rateFunction = function(rating) {
+      console.log('Rating selected: ' + rating);
+    };
   }
-  
-  $scope.sendRate = function(){
-    alert("Thanks for your rates!\n\nFirst Rate: " + $scope.ratings[0].current+"/"+$scope.ratings[0].max
-    +"\n"+"Second rate: "+ $scope.ratings[1].current+"/"+$scope.ratings[0].max)
-  }
-});
 
-starApp.directive('starRating', function () {
+  function starRating() {
     return {
-        restrict: 'A',
-        template: '<ul class="rating">' +
-            '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
-            '\u2605' +
-            '</li>' +
-            '</ul>',
-        scope: {
-            ratingValue: '=',
-            max: '=',
-            onRatingSelected: '&'
-        },
-        link: function (scope, elem, attrs) {
-
-            var updateStars = function () {
-                scope.stars = [];
-                for (var i = 0; i < scope.max; i++) {
-                    scope.stars.push({
-                        filled: i < scope.ratingValue
-                    });
-                }
-            };
-
-            scope.toggle = function (index) {
-                scope.ratingValue = index + 1;
-                scope.onRatingSelected({
-                    rating: index + 1
-                });
-            };
-
-            scope.$watch('ratingValue', function (oldVal, newVal) {
-                if (newVal) {
-                    updateStars();
-                }
-            });
+      restrict: 'EA',
+      template:
+        '<ul class="star-rating" ng-class="{readonly: readonly}">' +
+        '  <li ng-repeat="star in stars" class="star" ng-class="{filled: star.filled}" ng-click="toggle($index)">' +
+        '    <i class="fa fa-star"></i>' + // or &#9733
+        '  </li>' +
+        '</ul>',
+      scope: {
+        ratingValue: '=ngModel',
+        max: '=?', // optional (default is 5)
+        onRatingSelect: '&?',
+        readonly: '=?'
+      },
+      link: function(scope, element, attributes) {
+        if (scope.max == undefined) {
+          scope.max = 5;
         }
-    }
-});
+        function updateStars() {
+          scope.stars = [];
+          for (var i = 0; i < scope.max; i++) {
+            scope.stars.push({
+              filled: i < scope.ratingValue
+            });
+          }
+        };
+        scope.toggle = function(index) {
+          if (scope.readonly == undefined || scope.readonly === false){
+            scope.ratingValue = index + 1;
+            scope.onRatingSelect({
+              rating: index + 1
+            });
+          }
+        };
+        scope.$watch('ratingValue', function(oldValue, newValue) {
+          if (newValue) {
+            updateStars();
+          }
+        });
+      }
+    };
+  }
