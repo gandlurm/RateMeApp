7cd726dde50dc8b66a8f90202258ae4a4d7b0e1a
gandlurm <gandlurm@gmail.com>
2015-10-24 07:27:09 +0530

    Commit

--- static/scripts/controllers/ratingsPage.js
+++ static/scripts/controllers/ratingsPage.js
@@ -1,7 +1,7 @@
   'use strict';
 
   angular
-    .module('frontendApp', [])
+    .module('frontendApp')
     .controller('RatingController', RatingController)
     .directive('starRating', starRating);
 
