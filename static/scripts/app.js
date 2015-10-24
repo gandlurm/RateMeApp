6e5c7f2f9fb03679ba11304cddcf46a1a03e7641
gandlurm <gandlurm@gmail.com>
2015-10-24 22:16:47 +0530

    Commit

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -20,7 +20,7 @@ angular
       })
 	  .when('/ratingsPage', {
         templateUrl: 'static/views/ratingsPage.html',
-        controller: 'RatingController'
+        controller: 'appController'
       })
 	  .when('/fromfb', {
         templateUrl: 'static/views/fromfb.html',
